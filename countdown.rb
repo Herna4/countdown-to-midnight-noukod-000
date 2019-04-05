5c238ed6e68c2f008377da48efd44e8386367738
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-05 19:23:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+
+def countdown(integer)
+ 
+  while integer > 0
+  
+  puts "#{integer} SECOND(S)!"
+  integer -=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+ 
+  integer(5.minutes)
 end
